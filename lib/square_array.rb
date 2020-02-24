9ab49b6f93738911a9b14645e09c8dc814e8f95f
Deijah Price <deijahwilson1@gmail.com>
2020-02-24 22:15:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(numbers)
   counter=0 
   
   while numbers[counter] do
-    new_numbers=numbers[counter]**2
+  puts numbers[counter]**2
     counter += 1
-    puts square_array(new_numbers)
   end
 end
